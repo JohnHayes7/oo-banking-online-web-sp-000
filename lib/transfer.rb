4f191a191c1dee18b237126165572d8f51c781f6
John Hayes <johnnyhayes7@gmail.com>
2019-02-12 02:09:52 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -34,7 +34,8 @@ class Transfer
 
   def reverse_transfer
     if self.status == "complete"
-      
+      self.sender.balance -= self.amount
+      self.receiver.balance += self.amount
     end
     
   end
